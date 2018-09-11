27e8ac2ad81bcc87c4576152aa2dfcfb15449c4c
p-ganesh <41019754+p-ganesh@users.noreply.github.com>
2018-09-11 17:42:47 -0400

    eat your veggies

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbInput = new Scanner(System.in);
+	    System.out.println("What is your first name?");
+
+	    String firstName = kbInput.nextLine();
+
+	    System.out.println("What is your last name?");
+	    String lastName = kbInput.nextLine();
+
+	    System.out.println("Your full name is " + firstName + " " + lastName + ".");
     }
 }
